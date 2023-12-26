3eca2457915737606737686a733aca78ac2f1e7e
Maddy Santoso <maddysantoso@Maddys-MacBook-Air.local>
2023-12-26 00:27:14 -0500

    revised version

--- Diabetes.py
+++ Diabetes.py
@@ -53,6 +53,8 @@ sns.countplot(df, x='hypertension', order=hypertension_order).set_title('Hyperte
 sns.countplot(df, x='heart_disease', order=heart_disease_order).set_title('Heart Disease Distribution')
 sns.countplot(df, x='smoking_history', order=smoking_history_order).set_title('Smoking History Distribution')
 sns.countplot(df, x='diabetes', order=diabetes_order).set_title('Diabetes Distribution')
+
+# test and train the data set with algorithms: gradient boost, random forest and XGBoost
 X = df[['gender', 'age', 'hypertension', 'heart_disease', 'smoking_history', 'bmi', 'HbA1c_level', 'blood_glucose_level']]
 y = df['diabetes']
 X = pd.get_dummies(X, columns=['gender', 'smoking_history'], drop_first=True) # one hot encode
@@ -60,14 +62,41 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 df['binary_smoking_history'] = df['smoking_history'].apply(lambda x: 0 if x in ['no info', 'not current', 'never'] else 1) # binary labels for smoking history to use in class weights
 
-# create and fit model
+# class weights
+class_weights = compute_class_weight('balanced', classes=[0, 1], y=y_train)
+sample_weights = class_weights[y_train.astype(int)]
+
+# gradient boost
+gb_model = GradientBoostingClassifier(random_state=42)
+gb_model.fit(X_train, y_train, sample_weight=sample_weights)
+gb_pred = gb_model.predict(X_test) # predictions
+gb_cf = confusion_matrix(y_test, gb_pred)
+gb_accuracy = accuracy_score(y_test, gb_pred)
+
+# random forest
 rf_model = RandomForestClassifier(random_state=42, class_weight='balanced')
 rf_model.fit(X_train, y_train)
-# predictions
-y_pred = rf_model.predict(X_test)
-# check goodness of fit
-accuracy = accuracy_score(y_test, y_pred)
-# confusion matrix
-rf_cf = confusion_matrix(y_test, y_pred)
-print(f'Accuracy: {accuracy:.2f}')
-print(f'Confusion Matrix: \n{rf_cf}')
+rf_pred = rf_model.predict(X_test) # predictions
+rf_cf = confusion_matrix(y_test, rf_pred)
+rf_accuracy = accuracy_score(y_test, rf_pred)
+
+#XGBoost
+xgb_model = XGBClassifier(random_state=42, scale_pos_weight=(y_train == 0).sum() / (y_train == 1).sum(), enable_categorical=True)
+xgb_model.fit(X_train, y_train)
+xgb_pred = xgb_model.predict(X_test)
+xgb_cf = confusion_matrix(y_test,xgb_pred)
+xgb_accuracy = accuracy_score(y_test,xgb_pred)
+
+
+# print summary
+print("Gradient Boosting Classifier:")
+print(f"Confusion Matrix:\n{gb_cf}")
+print(f"Accuracy: {gb_accuracy:.2f}")
+
+print("\nRandom Forest Classifier:")
+print(f"Confusion Matrix:\n{rf_cf}")
+print(f"Accuracy: {rf_accuracy:.2f}")
+
+print("\nXGBoost Classifier:")
+print(f"Confusion Matrix:\n{xgb_cf}")
+print(f"Accuracy: {xgb_accuracy:.2f}")
