ac84b5baa2f4bb79952d0cbeb2f1b4da2c5ed127
Maddy Santoso <maddysantoso@Maddys-MacBook-Air.local>
2023-12-25 23:47:08 -0500

    updated diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -4,9 +4,11 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.utils import compute_class_weight
 
 
 df = pd.read_csv('diabetes_prediction_dataset.csv')
+
 # some data cleaning, factorizing and ordering
 df['gender'] = df['gender'].astype('category')
 gender_order = ['Female', 'Male', 'Other']
@@ -18,13 +20,8 @@ df['smoking_history'] = df['smoking_history'].astype('category')
 smoking_history_order = ['No Info', 'current', 'ever', 'former', 'never', 'not current']
 df['diabetes'] = df['diabetes'].astype('category')
 diabetes_order = [0, 1]
-
-# explanatory data analysis
-# show distribution of continuous variables: age, bmi, HbA1c_level, blood_glucose_level
-# using histogram
-def cont_distribution(var, pos):
+def cont_distribution(var):
     # histogram
-    plt.subplot(pos)
     plt.hist(df[var])
     plt.title(f'Histogram of {var}')
     plt.xlabel(var)
@@ -32,61 +29,45 @@ def cont_distribution(var, pos):
     plt.show()
 
     # box plot for outliers
-    plt.subplot(pos)
     sns.boxplot(x=df[var])
     plt.title(f'Box plot for {var}')
     plt.show()
 
 # subplot
 plt.figure(figsize=(16, 12))
-
-continuous_variables = ['age', 'bmi', 'HbA1c_level', 'blood_glucose_level']
-
-for i, variable in enumerate(continuous_variables, start=1):
-    cont_distribution(variable, 220 + i)
-
-plt.tight_layout()
-plt.show() # show the distribution of the variables
-
+cont_distribution('age')
+plt.show()
+cont_distribution('bmi')
+plt.show()
+cont_distribution('HbA1c_level')
+plt.show()
 # check Multicollinearity
 corr_matrix = df[['age', 'blood_glucose_level', 'HbA1c_level', 'bmi']]
 sns.heatmap(corr_matrix.corr(), annot=True)
 plt.title('Correlation Matrix')
 plt.show()
-# comment: No Multicollinearity, suggests a relatively low to moderate correlation between these variables.
-# comment: Not much outliers
 
-# Show distribution of categorical variables: gender, HTN, heart_disease, smoking_history, diabetes
-fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
-
-sns.countplot(df, x='gender', ax=axes[0, 0], order=gender_order)
-axes[0, 0].set_title('Gender Distribution')
-sns.countplot(df, x='hypertension', ax=axes[0, 1], order=hypertension_order)
-axes[0, 1].set_title('hypertension Distribution')
-sns.countplot(df, x='heart_disease', ax=axes[0, 2], order=heart_disease_order)
-axes[0, 2].set_title('heart_disease Distribution')
-sns.countplot(df, x='smoking_history', ax=axes[1, 0], order=smoking_history_order)
-axes[1, 0].set_title('smoking_history Distribution')
-sns.countplot(df, x='diabetes', ax=axes[1, 1], order=diabetes_order)
-axes[1, 1].set_title('diabetes Distribution')
-
-plt.tight_layout()
-plt.show()
-
-# I chose random forest to test and train my dataset with
-# Split the data into training and testing; 20% test, 80% train
+# for categorical variables
+sns.countplot(df, x='gender', order=gender_order).set_title('Gender Distribution')
+sns.countplot(df, x='hypertension', order=hypertension_order).set_title('Hypertension Distribution')
+sns.countplot(df, x='heart_disease', order=heart_disease_order).set_title('Heart Disease Distribution')
+sns.countplot(df, x='smoking_history', order=smoking_history_order).set_title('Smoking History Distribution')
+sns.countplot(df, x='diabetes', order=diabetes_order).set_title('Diabetes Distribution')
 X = df[['gender', 'age', 'hypertension', 'heart_disease', 'smoking_history', 'bmi', 'HbA1c_level', 'blood_glucose_level']]
 y = df['diabetes']
 X = pd.get_dummies(X, columns=['gender', 'smoking_history'], drop_first=True) # one hot encode
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-rf_model = RandomForestClassifier(random_state=42)
+
+df['binary_smoking_history'] = df['smoking_history'].apply(lambda x: 0 if x in ['no info', 'not current', 'never'] else 1) # binary labels for smoking history to use in class weights
+
+# create and fit model
+rf_model = RandomForestClassifier(random_state=42, class_weight='balanced')
 rf_model.fit(X_train, y_train)
 # predictions
 y_pred = rf_model.predict(X_test)
 # check goodness of fit
 accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy:.2f}')
 # confusion matrix
 rf_cf = confusion_matrix(y_test, y_pred)
-print(rf_cf)
+print(f'Accuracy: {accuracy:.2f}')
+print(f'Confusion Matrix: \n{rf_cf}')
